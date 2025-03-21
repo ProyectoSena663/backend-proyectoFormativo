e1e722c43bfdbe86a464e5fd58ec8d639e47470b
Ordogen <oarcilagarcia@gmail.com>
2025-03-21 17:26:30 -0500

    implementacion de los gets

--- index.ts
+++ index.ts
@@ -5,8 +5,24 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) =>{
-    res.send("hola typeScript con express!!")
-})
+//con este get obtenemos las prendas disponibles para personalizar.
+app.get("/prendas/:id", (req, res) =>{
+    res.send("GET de prendas")
+});
+
+//con este get obtenemos los colores dispononibles para personalizar.
+app.get("/colores", (req, res) =>{
+    res.send("GET de colores")
+});
+
+//con este get obtenemos los diseños disponibles para las personalizaciones.
+app.get("/disenhos", (req, res) =>{
+    res.send("GET de diseños")
+});
+
+//con este get obtenemos los diseños personalizados por los usuarios.
+app.get("/personalizaciones/:usuarioId", (req, res) =>{
+    res.send("GET de personalizaciones/:usuarioId")
+});
 
 app.listen(port, () => console.log(`servidor corriendo en el puerto ${port}`))
