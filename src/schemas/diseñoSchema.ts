a6dc15a56d3f9b3ce557597ebd0d37080845b02d
DiegoGuayara <diegofguayara9707@gmail.com>
2025-05-23 11:41:51 -0500

    Eliminé todo menos el sql

--- src/schemas/diseñoSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const diseñoSchema = z.object({
-  id_dis: z.number().int().positive().optional(),
-  fk_id_du: z.number().int().positive(),
-});
-
-export type diseñoType = z.infer<typeof diseñoSchema>;
