cc15d87db79d610874b082a383e7852fed2f9e37
brian <quinterobrian2403@gmail.com>
2025-05-28 19:51:04 -0500

    empezando el outfit

---
+++ microservicios/Outfit/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Outfit Service");
+});
+
+app.listen(PORT, () => {
+  console.log(`Outfit Service is running on http://localhost:${PORT}`);
+}); 
