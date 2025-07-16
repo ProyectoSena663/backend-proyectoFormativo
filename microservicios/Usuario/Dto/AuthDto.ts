a2b88e1e1b3b7e098a211aaf5415294c35639fc8
andressamuel13 <samuelma236@gmail.com>
2025-07-15 19:09:52 -0500

    class validator hecho en los dto de usuario

--- microservicios/Usuario/Dto/AuthDto.ts
+++ microservicios/Usuario/Dto/AuthDto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsString } from 'class-validator';
 export class AuthDto {
+
+  @IsEmail()
+  @IsString({ message: 'El email debe ser una cadena de texto' })
   email: string;
+
+  @IsString({ message: 'La contraseña debe ser una cadena de texto' })
   password: string;
 
   constructor(email: string, password: string) {
--- microservicios/Usuario/Dto/UserDto.ts
+++ microservicios/Usuario/Dto/UserDto.ts
@@ -1,25 +1,41 @@
-export class UserDto {
-  id_us?: string;
-  nombre: string;
-  apellido: string;
-  email: string;
-  fecha_nacimiento: Date;
-  red_social_login?: string;
-  password?: string;
+  import { IsEmail, IsOptional, IsString } from 'class-validator';
+  export class UserDto {
+    
+    @IsOptional()
+    id_us?: string;
 
-  constructor(
-    nombre: string,
-    apellido: string,
-    email: string,
-    fecha_nacimiento: Date,
-    red_social_login?: string,
-    password?: string
-  ) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.fecha_nacimiento = fecha_nacimiento;
-    this.red_social_login = red_social_login;
-    this.password = password;
+    @IsString({ message: 'El nombre debe ser una cadena de texto' })
+    nombre: string;
+
+    @IsString({ message: 'El apellido debe ser una cadena de texto' })
+    apellido: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsOptional()
+    fecha_nacimiento?: Date;
+
+    @IsOptional()
+    red_social_login?: string;
+
+    @IsOptional()
+    @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+    password?: string;
+
+    constructor(
+      nombre: string,
+      apellido: string,
+      email: string,
+      fecha_nacimiento: Date,
+      red_social_login?: string,
+      password?: string
+    ) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.email = email;
+      this.fecha_nacimiento = fecha_nacimiento;
+      this.red_social_login = red_social_login;
+      this.password = password;
+    }
   }
-}
